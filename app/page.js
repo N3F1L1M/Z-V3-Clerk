42fd1a4115b4e1e16228359c275bc43aeb161166
N3F1L1M <theblack50703@gmial.com>
2025-08-26 14:14:09 -0500

    add print id

--- app/page.js
+++ app/page.js
@@ -12,7 +12,7 @@ const page = async () => {
         Bienvenido { user?.username?.replace(/_/g, " ") // guiones bajos → espacios
             .replace(/\b\w/g, (c) => c.toUpperCase()) // primera letra en mayúscula
         }</div>
-      
+      <div>{user.id}</div>
 
       <div className="h-50 w-100 bg-[#a9350b] border-emerald-500 border-8"></div>
       <div className="h-50 w-100 bg-amber-500 border-emerald-500 border-8"></div>
