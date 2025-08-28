372677f4c0f66945404aa17013d60e9787676a1c
N3F1L1M <theblack50703@gmial.com>
2025-08-28 09:44:34 -0500

    FORMATEO
    se formateo de forma bonita todo el codigo del formulario para que este de una forma mas legible

--- app/productos/agregar/Formulario.tsx
+++ app/productos/agregar/Formulario.tsx
@@ -2,29 +2,34 @@
 import axios from "axios";
 import { useState, ChangeEvent, FormEvent } from "react";
 
-export default function Home() {
-  //variables para el manejo de las imagenes que se agregan al formulario. Es un arreglo de archivos para permitir mas de una imagen
-  const [selectedImages, setSelectedImages] = useState<File[]>([]);
 
-  //Variables que son las guardan las urls de las imagenes que se carguen
-  const [images, setImages] = useState<string[]>([]);
 
-  //Variables que se usan simplemente para mostrar los mensajes del proceso de envio al S3 e.j: "Procesando..."
-  const [charge, setCharge] = useState<string>("")
+export default function formulario() {
 
-  const handleImageChange = (e: ChangeEvent<HTMLInputElement>) => {
-    if (e.target.files) {
-      const filesArray = Array.from(e.target.files);
-      setSelectedImages(filesArray);
-    }
-  };
 
-  //Metodo para cuando se le de al boton de Cargar
-  const submit = async (e: FormEvent<HTMLFormElement>) => {
+  //VARIABLES o ESTADOS (Hook de react)
+  const [selectedImages, setSelectedImages] = useState<File[]>([]);//imagenes que se agregan al formulario. 
+  const [images, setImages] = useState<string[]>([]);//guardan las urls de las imagenes que se carguen
+  const [charge, setCharge] = useState<string>("");//mensajes del proceso de envio al S3 e.j: "Procesando..."
+
+
+
+
+  //fUNCIONES MANEJADORAS DE EVENTOS 
+
+  const handleImageChange = (e: ChangeEvent<HTMLInputElement>) => {//agrega las imagenes a el arreglo
+    if (e.target.files) { const filesArray = Array.from(e.target.files); 
+      setSelectedImages(filesArray);}};
+      
+     
+
+
+
+  const submit = async (e: FormEvent<HTMLFormElement>) => {//maneja el boton de submit 
     e.preventDefault();
     console.log(selectedImages);
-    try {
-      //Se verifica que el campo de imagenes tenga al menos 1 imagen
+
+    try {//Se verifica que el campo de imagenes tenga al menos 1 imagen
       if (selectedImages.length > 0) {
         setCharge("Procesando...");
         const formData = new FormData();
@@ -34,13 +39,10 @@ export default function Home() {
           formData.append("images", image); 
         });
 
-        //Axios es la libreria que permite realizar peticiones HTTP, por lo que se envia los datos que se agregaron al formulario a la ruta de api/s3
+        //se envia los datos que se agregaron al formulario a la ruta de api/s3
+        //Para que la peticion acepte imagenes y no texto, especificamos que el contentType sea multipart/form-data
         const { data } = await axios.post("/api/s3", formData, {
-          //Para que la peticion acepte imagenes y no texto, especificamos que el contentType sea multipart/form-data
-          headers: {
-            "Content-Type": "multipart/form-data",
-          },
-        });
+          headers: {"Content-Type": "multipart/form-data",},});
 
         setCharge("Enviando a Bucket...");
 
@@ -53,6 +55,7 @@ export default function Home() {
           setCharge("Se han cargado las imagenes exitosamente");
         }
       }
+      
     } catch (error) {
       console.log(error);
       setCharge("Ha ocurrido un error...");
@@ -60,35 +63,35 @@ export default function Home() {
   }
 
   return (
+
     <div className="max-w-md mx-auto p-6 bg-gray-100 rounded-lg shadow-md">
       <h2 className="text-2xl font-bold mb-4 text-center">Añadir Producto</h2>
       {/* {if (images){
         <h2>Se han cargado las imagenes exitosamente</h2>
       }} */}
-      <form className="space-y-4" onSubmit={submit}>
-        <input
-          type="text"
-          placeholder="Título del producto"
-          className="w-full p-2 border rounded"
-        />
 
-        <input
-          type="number"
-          step="0.01"
-          placeholder="0.0"
-          className="w-full p-2 border rounded"
-        />
+
+      <form className="space-y-4" onSubmit={submit}>
+
+        <input type="text" placeholder="Título del producto"
+          className="w-full p-2 border rounded"/>
+
+        <input type="number" step="0.01" placeholder="0.0"
+          className="w-full p-2 border rounded"/>
+
+
+
 
         <div>
+              {/* Input de las imagenes, cada vez que se carguen imagenes, este llamara la funcion de handleImageChange para llevar una lista viva de las imagenes que se estan cargando */}
           <label className="block mb-1 font-medium">Imágenes del producto</label>
-          {/* Input de las imagenes, cada vez que se carguen imagenes, este llamara la funcion de handleImageChange para llevar una lista viva de las imagenes que se estan cargando */}
-          <input
-            type="file"
-            accept="image/*"
-            multiple
-            onChange={handleImageChange}
-            className="w-full p-2 border rounded bg-white"
-          />
+
+          <input type="file" accept="image/*" multiple onChange={handleImageChange}
+            className="w-full p-2 border rounded bg-white"/>
+
+
+
+
           {/* Elemento que muestra visualmente las imagenes que se han agregado al formulario */}
           {selectedImages.length > 0 && (
             <div className="flex flex-wrap gap-2 mt-2">
@@ -105,30 +108,26 @@ export default function Home() {
           )}
         </div>
 
-        <input
-          type="text"
-          placeholder="Detalles"
-          className="w-full p-2 border rounded"
-        />
 
-        <textarea
-          placeholder="Descripción"
-          className="w-full p-2 border rounded resize-none"
-        />
 
-        <button
-          type="submit"
-          className="w-full bg-indigo-500 hover:bg-indigo-600 text-white py-2 rounded"
-        >
-          Cargar Producto
-        </button>
-        <input
-          type="reset"
-          placeholder="Reset"
-          className="w-full bg-indigo-500 hover:bg-indigo-600 text-white py-2 rounded"
-        />
+        <input type="text" placeholder="Detalles"
+          className="w-full p-2 border rounded"/>
+
+        <textarea placeholder="Descripción"
+          className="w-full p-2 border rounded resize-none"/>
+        
+
+        <button type="submit" 
+          className="w-full bg-indigo-500 hover:bg-indigo-600 text-white py-2 rounded">
+          Cargar Producto </button>
+          
+        <input type="reset"placeholder="Reset"
+          className="w-full bg-indigo-500 hover:bg-indigo-600 text-white py-2 rounded"/>
+        
       </form>
-      <h2 className="text-2xl font-bold mb-4 text-center">{charge}</h2>
+
+      <h2 className="text-2xl font-bold mb-4 text-center">{charge}</h2> 
+
     </div>
   );
 }
