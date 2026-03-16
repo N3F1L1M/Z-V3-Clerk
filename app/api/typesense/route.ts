0425c269e1a53da19ad75236736747383ae096f9
N3F1L1M <theblack50703@gmail.com>
2026-03-15 21:09:53 -0500

    se crea un endpoint para cuidar la base de datos

---
+++ app/api/typesense/route.ts
@@ -0,0 +1,54 @@
+
+import { NextRequest, NextResponse } from "next/server";
+import { auth } from "@clerk/nextjs/server";
+import { client } from '@/lib/Typesense_client';
+
+
+//FUNCION GET 
+export async function GET(req: NextRequest) {
+
+
+
+    //se carga los datos de sesion 
+     const { userId } = await auth();
+
+    try {
+        //se revisa que el usuario este loggeado
+        if (!userId) {return NextResponse.json({message: "Error: No autorizado. Debes iniciar sesión."}, { status: 401 });}
+
+
+        const query = req.nextUrl.searchParams.get("q");
+        const page = req.nextUrl.searchParams.get("page");
+
+
+        //FUNCION BUSCADORA TYPESENSE
+        const searchParameters = {
+              q: query || "*",
+              query_by: "titulo",
+              filter_by: `id_tienda:=${userId}`,
+              page: Number(page) || 1,
+              sort_by: "_text_match:desc"};
+
+
+         const results = await client
+              .collections('productos')
+              .documents()
+              .search(searchParameters);
+              return NextResponse.json({ results }); // Devuelve los resultados de la búsqueda en formato JSON
+        
+          
+        //FUNCION BUSCADORA TYPESENSE
+        
+    } 
+    
+    
+    
+    catch (error) { 
+        return NextResponse.json({message: "Error: en la busqueda."}, { status:500 }); }
+        
+            
+            
+            
+       
+    
+}
